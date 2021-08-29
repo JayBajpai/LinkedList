8045d91e6793226db172b94e81f067758c20bfa7
Jay22031999 <Jay22031999>
2021-08-29 15:18:24 +0530

    added simple linked list

--- LinkedListProgram/src/linkedlist/MyNode.java
+++ LinkedListProgram/src/linkedlist/MyNode.java
@@ -1,13 +1,13 @@
 package linkedlist;
 
 public class MyNode<K> implements INode<K> {
-	private K key=null;
-	private INode next=null;
 	
+	private K key=null;
+	private INode next=null;	
 	public MyNode(K key) {
 		this.key = key;
 		this.next = null;
-	}
+	}	
 	@Override
 	public K getKey() {
 		// TODO Auto-generated method stub
@@ -23,5 +23,5 @@ public class MyNode<K> implements INode<K> {
 	}
 	public void setNext(INode next) {
 		this.next = (MyNode<K>)next;
-	}	
+	}
 }
